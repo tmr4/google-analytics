b88be80764aea07a51011cb009c8aa82c71bb9c7
tmrob4 <tmrob4@gmail.com>
2020-07-04 14:38:09 -0700

    cleanup

--- google-analytics.php
+++ google-analytics.php
@@ -3,6 +3,7 @@
 Plugin Name: Google Analytics
 Version: 3
 */
+
 // WordPress seems to confuse this plugin with another and notes it needs updated unless my plugin has a higher version number
 // *** Find out what WordPress uses to diferentiate plugins as changing Plugin Name doesn't seem to solve this problem ***
 
@@ -10,16 +11,16 @@ Version: 3
 // IP anonymization modification from: https://developers.google.com/analytics/devguides/collection/gtagjs/ip-anonymization
 function psp_add_googleanalytics() { ?>
      
-<!-- Global site tag (gtag.js) - Google Analytics - IP Anonymized -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171530527-1"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics - IP Anonymized -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171530527-1"></script>
 
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){ dataLayer.push(arguments); }
-  gtag('js', new Date());
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
 
-  gtag( 'config', 'UA-171530527-1', { 'anonymize_ip' : true } );
-</script>
+    gtag( 'config', 'UA-171530527-1', { 'anonymize_ip' : true } );
+  </script>
  
 <?php }
 add_action('wp_head', 'psp_add_googleanalytics');
